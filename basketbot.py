2776116d919ae1598fbce3e143e94cfab0e587e1
Paramoshka4 <92852060+Paramoshka4@users.noreply.github.com>
2021-10-20 14:47:57 +0200

    Update basketbot.py

--- basketbot.py
+++ basketbot.py
@@ -14,7 +14,7 @@ longpoll = VkLongPoll(authorize)
 
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
-        ed = 144915961
+        dima = 232515926
         lesha = 560170062
         received_message = event.text
         rm = received_message.lower()
@@ -23,7 +23,7 @@ for event in longpoll.listen():
             count = int(rm[12:])
             i = 0
             people = []
-            send_message(ed, "Набор на тренировку начался")            
+            send_message(dima, "Набор на тренировку начался")            
             send_message(lesha, "Набор на тренировку начался")
         if rm == "список":
             itog1 = ""
@@ -58,4 +58,4 @@ for event in longpoll.listen():
                         a += 1
                     if i == (count - 1):
                         send_message(lesha, itog)
-                        send_message(ed, itog)
+                        send_message(dima, itog)
